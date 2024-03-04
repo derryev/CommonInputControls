5a376adb083e10108ec9bc5d966ceb04589709eb
derryev <derryev@ssfs.org>
2024-03-04 15:06:04 -0500

    finished common input controls lab

--- CommonInputControls/ViewController.swift
+++ CommonInputControls/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var toggle: UISwitch!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Button was tapped!")
+        if toggle.isOn {
+            print("The switch is on!")
+        } else {
+            print("The switch is off.")
+        }
+        print("The slider is set to \(slider.value)")
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("The switch is on!")
+        } else {
+            print("The switch is off.")
+        }
+    }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    @IBAction func textChanged(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in:view)
+        print(location)
+    }
 }
 
